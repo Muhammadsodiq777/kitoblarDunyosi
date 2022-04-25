37dfe5cd00300822a67bb15848413ded09071bda
Muhammadsodiq777 <m.muhammadsodiq777@gmail.com>
2022-04-25 15:42:14 +0500

    lfkhsd

--- BookServiice/src/main/java/uz/pdp/bookserviice/controller/BookController.java
+++ BookServiice/src/main/java/uz/pdp/bookserviice/controller/BookController.java
@@ -2,6 +2,7 @@ package uz.pdp.bookserviice.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +21,8 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping("/all")
-    public HttpEntity getAllBook(){
-        List<BookEntity> bookDTOList = bookService.getAllBooks();
-        return null;
+    public ResponseEntity getAllBook(){
+        List<BookEntity> list = bookService.getAllBooks();
+        return ResponseEntity.ok(list);
     }
 }
